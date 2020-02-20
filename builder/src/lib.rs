2d16ddf4ed06d61773d0c0b4502419bf6e74b753
ymgyt <yamaguchi7073xtt@gmail.com>
2020-02-20 19:16:55 +0900

    pass 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -3,8 +3,6 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(_input: TokenStream) -> TokenStream {
+    TokenStream::new()
 }
